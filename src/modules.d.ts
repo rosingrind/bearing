35603bb99b8809b4497ff0fd3b9305e9da988054
mesame8616 <72046007+mesame8616@users.noreply.github.com>
2020-09-28 23:11:33 +0300

    Update modules.d.ts

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,24 +1,24 @@
 declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const data: { [key: string]: string };
+  export default data;
 }
 
 declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const data: { [key: string]: string };
+  export default data;
 }
 
 declare module '*.module.sass' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const data: { [key: string]: string };
+  export default data;
 }
 
 declare module '*.module.less' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const data: { [key: string]: string };
+  export default data;
 }
 
 declare module '*.module.styl' {
-  const classes: { [key: string]: string };
-  export default classes;
+  const data: { [key: string]: string };
+  export default data;
 }
