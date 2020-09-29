4ffa382ae107f84b97b6ae904030cd908355834a
andrewbrodko <work.andrewbrodko@gmail.com>
2020-09-29 14:48:16 +0300

    Config changes
    - moved declaration generation from package.json stage to rollup stage
    - separate configs for main package and easings package

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss-modules';
 import autoprefixer from 'autoprefixer';
-import pkg from './package.json';
 
-export default {
-  input: 'src/index.tsx',
+const bearing = {
+  input: './src/index.tsx',
   output: {
-    file: pkg.main,
+    dir: './dist',
     format: 'cjs',
     exports: 'named',
     sourcemap: true,
@@ -17,7 +16,26 @@ export default {
       extract: false,
       writeDefinitions: false,
     }),
-    typescript(),
+    typescript({
+      outDir: './dist',
+      declaration: true,
+      declarationDir: './dist',
+    }),
   ],
   external: ['react', 'react-dom', 'classnames/bind'],
 };
+
+const easings = {
+  input: './src/easings/index.ts',
+  output: {
+    dir: './dist/easings',
+    format: 'cjs',
+    exports: 'named',
+    sourcemap: true,
+  },
+  plugins: [typescript({ outDir: './dist/easings' })],
+};
+
+const configs = [bearing, easings];
+
+export default configs;
