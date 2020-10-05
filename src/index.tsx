e4f9422b9ad11f126298e12e6357ec0c48f1d95c
Andrew Brodko <work.andrewbrodko@gmail.com>
2020-10-05 11:26:32 +0300

    Element positioning and overlaying changes (#18)
    * Update index.tsx
    
    * Element positioning and overlaying changes
    
    - control buttons are now in contents-displayed span
    - removed unnecessary sizing span

--- src/index.tsx
+++ src/index.tsx
@@ -18,8 +18,6 @@ const Carousel: React.FC<{
     interval?: number;
   };
 }> = ({ slides: data, size, animation, animation: { interval } }) => {
-  console.log(size);
-
   const [current, setCurrent] = useState(0);
   const [move, setMove] = useState(0);
   const [slides, setSlides] = useState(data);
@@ -75,15 +73,14 @@ const Carousel: React.FC<{
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.carousel}>
+      <div
+        className={styles.carousel}
+        style={{
+          width: `${size.width}px`,
+          height: `${size.height}px`,
+        }}
+      >
         <div>
-          <span
-            style={{
-              visibility: 'hidden',
-              width: `${size.width}px`,
-              height: `${size.height}px`,
-            }}
-          ></span>
           {slides.map((s, key) => (
             <Slide
               key={key}
