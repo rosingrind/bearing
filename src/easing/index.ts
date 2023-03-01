2bb6bcdf276ec3dc9c1cb94d62b9ddc8957f29bc
Adam Barton <108860307+rosingrind@users.noreply.github.com>
2023-03-01 04:58:02 +0400

    Coverage for easing functions

--- src/easing/index.ts
+++ src/easing/index.ts
@@ -37,26 +37,48 @@ export const easeOutCubic = (x: number) => 1 - (1 - x) ** 3;
 export const easeInOutCubic = (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - (-2 * x + 2) ** 3 / 2);
 export const easeInQuart = (x: number) => x * x * x * x;
 export const easeOutQuart = (x: number) => 1 - (1 - x) ** 4;
-export const easeInOutQuart = (x: number) => (x < 0.5 ? 8 * x * x * x * x : 1 - (-2 * x + 2) ** 4 / 2);
+export const easeInOutQuart = (x: number) => (x < 0.5
+  ? 8 * x * x * x * x
+  : 1 - (-2 * x + 2) ** 4 / 2);
 export const easeInQuint = (x: number) => x * x * x * x * x;
 export const easeOutQuint = (x: number) => 1 - (1 - x) ** 5;
-export const easeInOutQuint = (x: number) => (x < 0.5 ? 16 * x * x * x * x * x : 1 - (-2 * x + 2) ** 5 / 2);
+export const easeInOutQuint = (x: number) => (x < 0.5
+  ? 16 * x * x * x * x * x
+  : 1 - (-2 * x + 2) ** 5 / 2);
 export const easeInSine = (x: number) => 1 - cos((x * PI) / 2);
 export const easeOutSine = (x: number) => sin((x * PI) / 2);
 export const easeInOutSine = (x: number) => -(cos(PI * x) - 1) / 2;
 export const easeInExpo = (x: number) => (x === 0 ? 0 : 2 ** (10 * x - 10));
 export const easeOutExpo = (x: number) => (x === 1 ? 1 : 1 - 2 ** (-10 * x));
-export const easeInOutExpo = (x: number) => (x === 0 ? 0 : x === 1 ? 1 : x < 0.5 ? 2 ** (20 * x - 10) / 2 : (2 - 2 ** (-20 * x + 10)) / 2);
+export const easeInOutExpo = (x: number) => (x === 0
+  ? 0
+  : x === 1
+    ? 1
+    : x < 0.5
+      ? 2 ** (20 * x - 10) / 2
+      : (2 - 2 ** (-20 * x + 10)) / 2);
 export const easeInCirc = (x: number) => 1 - sqrt(1 - x ** 2);
 export const easeOutCirc = (x: number) => sqrt(1 - (x - 1) ** 2);
-export const easeInOutCirc = (x: number) => (x < 0.5 ? (1 - sqrt(1 - (2 * x) ** 2)) / 2 : (sqrt(1 - (-2 * x + 2) ** 2) + 1) / 2);
+export const easeInOutCirc = (x: number) => (x < 0.5
+  ? (1 - sqrt(1 - (2 * x) ** 2)) / 2
+  : (sqrt(1 - (-2 * x + 2) ** 2) + 1) / 2);
 export const easeInBack = (x: number) => c3 * x * x * x - c1 * x * x;
-export const easeOutBack = (x: number) => 1 + c3 * (x - 1) ** 3 + c1 * (x - 1) ** 2;
+export const easeOutBack = (x: number) => (x === 0
+  ? 0 // float-precision
+  : 1 + c3 * (x - 1) ** 3 + c1 * (x - 1) ** 2);
 export const easeInOutBack = (x: number) => (x < 0.5
   ? ((2 * x) ** 2 * ((c2 + 1) * 2 * x - c2)) / 2
   : ((2 * x - 2) ** 2 * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2);
-export const easeInElastic = (x: number) => (x === 0 ? 0 : x === 1 ? 1 : -(2 ** (10 * x - 10)) * sin((x * 10 - 10.75) * c4));
-export const easeOutElastic = (x: number) => (x === 0 ? 0 : x === 1 ? 1 : 2 ** (-10 * x) * sin((x * 10 - 0.75) * c4) + 1);
+export const easeInElastic = (x: number) => (x === 0
+  ? 0
+  : x === 1
+    ? 1
+    : -(2 ** (10 * x - 10)) * sin((x * 10 - 10.75) * c4));
+export const easeOutElastic = (x: number) => (x === 0
+  ? 0
+  : x === 1
+    ? 1
+    : 2 ** (-10 * x) * sin((x * 10 - 0.75) * c4) + 1);
 export const easeInOutElastic = (x: number) => (x === 0
   ? 0
   : x === 1
@@ -66,4 +88,6 @@ export const easeInOutElastic = (x: number) => (x === 0
       : (2 ** (-20 * x + 10) * sin((20 * x - 11.125) * c5)) / 2 + 1);
 export const easeInBounce = (x: number) => 1 - bounceOut(1 - x);
 export const easeOutBounce = bounceOut;
-export const easeInOutBounce = (x: number) => (x < 0.5 ? (1 - bounceOut(1 - 2 * x)) / 2 : (1 + bounceOut(2 * x - 1)) / 2);
+export const easeInOutBounce = (x: number) => (x < 0.5
+  ? (1 - bounceOut(1 - 2 * x)) / 2
+  : (1 + bounceOut(2 * x - 1)) / 2);
